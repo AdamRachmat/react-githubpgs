6a9bcd941fb912ed19b0a33d24ccfb36761126dd
Adam Rachmat <adambarata@gmail.com>
2023-03-09 20:29:09 +0700

    perbaikan qrcode canvas

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import QRCode from 'qrcode.react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
     <App />
-  
+);
+ReactDOM.render(
+    <QRCode value="https://reactjs.org/" renderAs="canvas" />,
+    document.getElementById('mountNode')
 );
 
 // If you want to start measuring performance in your app, pass a function
